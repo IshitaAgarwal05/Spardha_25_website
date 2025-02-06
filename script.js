66bc117b4f4d9af6615314607a13866b3faaea91
Ishita <2005.agarwal.ishita@gmail.com>
2025-02-06 09:39:10 +0530

    update 7

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+function toggleMenu() {
+  var menu = document.getElementById("mobileMenu");
+  if (menu.classList.contains("w3-hide")) {
+    menu.classList.remove("w3-hide");
+  } else {
+    menu.classList.add("w3-hide");
+  }
+}
+
 // Hide the loader once the page is fully loaded
 window.addEventListener("load", function () {
   let loader = document.getElementById("loader");
   setTimeout(() => {loader.style.display = "none";}, 3000);
 });
-
-
-
-
-
-
-
 
 // scroll countdown numbers
 function startCountdownAnimation(id, finalValue) {
@@ -228,21 +230,6 @@ form.addEventListener('submit', (e) => {
   .then(response => alert("Thank you! Your form is submitted successfully!"))
   // .then(() => {window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
-
-  // var formData = new FormData(form);
-  //       var gender = document.getElementById("gender").checked;
-  //       var domain = document.getElementById("domain").checked;
-
-  //       formData.append("Gender", gender);
-  //       formData.append("Domain", domain);
-
-  //       fetch(scriptURL, { method: "POST", body: formData })
-  //         .then((response) => {
-  //           swal("Done", "Submitted Successfully.", "success");
-  //         })
-  //         .catch((error) => {
-  //           swal("Error", "Something went wrong. please try again!", "error");
-  //         });
 })
 
 
@@ -265,3 +252,132 @@ function toggleMap() {
 
 toggleMap();
 window.addEventListener('resize', toggleMap);
+
+
+
+
+
+
+
+
+
+// // media scroll effect
+// /*--------------------
+// Vars
+// --------------------*/
+// const $menu = document.querySelector('.menu')
+// const $items = document.querySelectorAll('.menu--item')
+// const $images = document.querySelectorAll('.menu--item img')
+// let menuWidth = $menu.clientWidth
+// let itemWidth = $items[0].clientWidth
+// let wrapWidth = $items.length * itemWidth
+
+// let scrollSpeed = 0
+// let oldScrollY = 0
+// let scrollY = 0
+// let y = 0
+
+
+// /*--------------------
+// Lerp
+// --------------------*/
+// const lerp = (v0, v1, t) => {
+//   return v0 * ( 1 - t ) + v1 * t
+// }
+
+
+// /*--------------------
+// Dispose
+// --------------------*/
+// const dispose = (scroll) => {
+//   gsap.set($items, {
+//     x: (i) => {
+//       return i * itemWidth + scroll
+//     },
+//     modifiers: {
+//       x: (x, target) => {
+//         const s = gsap.utils.wrap(-itemWidth, wrapWidth - itemWidth, parseInt(x))
+//         return `${s}px`
+//       }
+//     }
+//   })
+// } 
+// dispose(0)
+
+
+// /*--------------------
+// Wheel
+// --------------------*/
+// const handleMouseWheel = (e) => {
+//   scrollY -= e.deltaY * 0.9
+// }
+
+
+// /*--------------------
+// Touch
+// --------------------*/
+// let touchStart = 0
+// let touchX = 0
+// let isDragging = false
+// const handleTouchStart = (e) => {
+//   touchStart = e.clientX || e.touches[0].clientX
+//   isDragging = true
+//   $menu.classList.add('is-dragging')
+// }
+// const handleTouchMove = (e) => {
+//   if (!isDragging) return
+//   touchX = e.clientX || e.touches[0].clientX
+//   scrollY += (touchX - touchStart) * 2.5
+//   touchStart = touchX
+// }
+// const handleTouchEnd = () => {
+//   isDragging = false
+//   $menu.classList.remove('is-dragging')
+// }
+
+
+// /*--------------------
+// Listeners
+// --------------------*/
+// $menu.addEventListener('mousewheel', handleMouseWheel)
+
+// $menu.addEventListener('touchstart', handleTouchStart)
+// $menu.addEventListener('touchmove', handleTouchMove)
+// $menu.addEventListener('touchend', handleTouchEnd)
+
+// $menu.addEventListener('mousedown', handleTouchStart)
+// $menu.addEventListener('mousemove', handleTouchMove)
+// $menu.addEventListener('mouseleave', handleTouchEnd)
+// $menu.addEventListener('mouseup', handleTouchEnd)
+
+// $menu.addEventListener('selectstart', () => { return false })
+
+
+// /*--------------------
+// Resize
+// --------------------*/
+// window.addEventListener('resize', () => {
+//   menuWidth = $menu.clientWidth
+//   itemWidth = $items[0].clientWidth
+//   wrapWidth = $items.length * itemWidth
+// })
+
+
+// /*--------------------
+// Render
+// --------------------*/
+// const render = () => {
+//   requestAnimationFrame(render)
+//   y = lerp(y, scrollY, .1)
+//   dispose(y)
+  
+//   scrollSpeed = y - oldScrollY
+//   oldScrollY = y
+  
+//   gsap.to($items, {
+//     skewX: -scrollSpeed * .2,
+//     rotate: scrollSpeed * .01,
+//     scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * 0.003
+//   })
+// }
+// render()
