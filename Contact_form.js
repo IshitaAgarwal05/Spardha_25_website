7746333717030295cc323ea847d2f77c7e0f7f74
Ishita <2005.agarwal.ishita@gmail.com>
2025-02-13 16:22:17 +0530

    final update 14

--- Contact_form.js
+++ Contact_form.js
@@ -1,38 +1,3 @@
-// Gender Selection
-// Function to display game selection based on the domain chosen
-function showGameSelection() {
-    const domain = document.getElementById("domain").value;
-    const gameSelection = document.getElementById("game");
-
-    gameSelection.innerHTML = ""; // Clear previous options
-
-    if (domain === "E-Sports") {
-        gameSelection.style.display = 'block';
-        gameSelection.style.marginTop = '20px';
-        gameSelection.innerHTML = `
-            <option value="">Select Game</option>
-            <option value="BGMI">BGMI</option>
-        `;
-    } else if (domain === "Flagship") {
-        gameSelection.style.display = 'block';
-        gameSelection.style.marginTop = '20px';
-        gameSelection.innerHTML = `
-            <option value="">Select Game</option>
-            <option value="Cricket">Cricket</option>
-            <option value="Volleyball">Volleyball</option>
-            <option value="Basketball">Basketball</option>
-            <option value="Futsal Football">Futsal Football</option>
-            <option value="7-a-side Football">7-a-side Football</option>
-            <option value="Badminton">Badminton</option>
-            <option value="Table Tennis">Table Tennis</option>
-        `;
-    } else {
-        gameSelection.style.display = 'none';
-    }
-
-    document.getElementById("gameSelection").style.display = (domain !== "") ? "block" : "none";
-}
-
 // Send details via mail
 function sendMail() {
     let parms = {
@@ -41,7 +6,6 @@ function sendMail() {
         email: document.getElementById("email").value,
         university: document.getElementById("university").value,
         city: document.getElementById("city").value,
-        domain: document.getElementById("domain").value,
         game: document.getElementById("game").value
     };
 
